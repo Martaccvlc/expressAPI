eb4b0552c43fa17c2c35f7c4e02d0f444c314f57
martaccvlc <catpucchinobots@proton.me>
2025-02-10 00:58:02 +0100

    Added centralized logging

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express, { Request, Response } from "express";
 
+import Logger from "./utils/logger";
+import morganMiddleware from './config/morganMiddleware'
+
 // Create a new express application instance
 const app = express();
 
 // Set the network port
 const port = process.env.PORT || 3000;
 
+// Add Morgan middleware
+app.use(morganMiddleware)
+
 // Define the root path with a greeting message
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Welcome to the Express + TypeScript Server!" });
+    Logger.info("Success");
 });
 
 // Start the Express server
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
+    Logger.info("Express server is running.");
 });
